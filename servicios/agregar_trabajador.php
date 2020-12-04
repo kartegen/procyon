ed3b96bab3ad64f83e4c7dc2560710e3fe2641d2
jesus rlv <jesusrlv@iMac-de-jesus.local>
2020-12-04 10:59:27 -0600

    Fechas en input

--- servicios/agregar_trabajador.php
+++ servicios/agregar_trabajador.php
@@ -303,10 +303,19 @@
                   <label class="input-group-text" for="inputGroupSelect01">Mes</label>
                 </div>
                 <select class="custom-select" id="inputGroupSelect01">
-                  <option selected>Seleccionar...</option>
-                  <option value="1">One</option>
-                  <option value="2">Two</option>
-                  <option value="3">Three</option>
+                <option selected>Seleccionar...</option>
+                  <option value="1">Enero</option>
+                  <option value="2">Febrero</option>
+                  <option value="3">Marzo</option>
+                  <option value="4">Abril</option>
+                  <option value="5">Mayo</option>
+                  <option value="6">Junio</option>
+                  <option value="7">Julio</option>
+                  <option value="8">Agosto</option>
+                  <option value="9">Septiembre</option>
+                  <option value="10">Octubre</option>
+                  <option value="11">Noviembre</option>
+                  <option value="12">Diciembre</option>
                 </select>
               </div>
 
