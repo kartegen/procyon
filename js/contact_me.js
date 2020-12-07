d45798821e863fb7ef4426f420d38478f0fb29a8
Luis <Luis@Luis-PC>
2020-12-06 21:24:44 -0600

    Contacto

--- js/contact_me.js
+++ js/contact_me.js
@@ -26,7 +26,6 @@ $(function() {
                 },
                 cache: false,
                 success: function() {
-                	swal("Here's a message!")
                     // Success message
                     $('#success').html("<div class='alert alert-success'>");
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
